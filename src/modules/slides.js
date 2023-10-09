dc91e192a2bdf8da081e54c9f1fc126ba88a7fbc
MirashKG <kypchakbaev.abdukarim@gmail.com>
2023-10-09 19:33:35 +0600

    update

--- src/modules/slides.js
+++ src/modules/slides.js
@@ -3,15 +3,7 @@ const brandsWrapper = document.querySelector('.brands__wrapper');
 const item = document.querySelectorAll('.brands__item')
 const brandsBtn = document.querySelector('.brands__btn')
 
-if (window.innerWidth <= 320) {
-	slider.classList.toggle('swiper-container')
-	brandsWrapper.classList.toggle('swiper-wrapper')
-	item.forEach(element => {
-		element.classList.toggle('swiper-slide')
-	});
-	brandsBtn.remove()
-	sliderSwiper()
-}
+
 
 function sliderSwiper() {
 	new Swiper(wrapper, {
@@ -25,4 +17,14 @@ function sliderSwiper() {
 			el: ".swiper-pagination",
 		  },
 	})
+}
+
+if (window.innerWidth <= 320) {
+	wrapper.classList.toggle('swiper-container')
+	brandsWrapper.classList.toggle('swiper-wrapper')
+	item.forEach(element => {
+		element.classList.toggle('swiper-slide')
+	});
+	brandsBtn.remove()
+	sliderSwiper()
 }
